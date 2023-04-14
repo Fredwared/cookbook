ebca47762ad25217138874cd124200ef495ac279
Golib <fredwared@gmail.com>
2023-04-14 17:19:06 +0500

    Docs update

--- app/Http/Controllers/Api/V1/Products/CurrencyController.php
+++ app/Http/Controllers/Api/V1/Products/CurrencyController.php
@@ -23,7 +23,7 @@ class CurrencyController extends Controller
      * @return AnonymousResourceCollection
      *
      * @apiResource App\Http\Resources\V1\Products\CurrencyResource
-     * @apiResourceModel App\Models\CurrencyService
+     * @apiResourceModel App\Services\Products\CurrencyService
      *
      */
     public function index(): AnonymousResourceCollection
@@ -50,7 +50,7 @@ class CurrencyController extends Controller
      *
      *
      * @apiResource App\Http\Resources\V1\Products\CurrencyResource
-     * @apiResourceModel App\Models\CurrencyService
+     * @apiResourceModel App\Services\Products\CurrencyService
      */
     public function store(StoreCurrencyRequest $request, CurrencyService $currencyService): JsonResponse
     {
@@ -78,7 +78,7 @@ class CurrencyController extends Controller
      * @return CurrencyResource
      *
      * @apiResource App\Http\Resources\V1\Products\CurrencyResource
-     * @apiResourceModel App\Models\CurrencyService
+     * @apiResourceModel App\Services\Products\CurrencyService
      */
     public function show(Currency $currency): CurrencyResource
     {
@@ -117,7 +117,7 @@ class CurrencyController extends Controller
      * @return AnonymousResourceCollection
      *
      * @apiResource App\Http\Resources\V1\Products\ListCurrencyResource
-     * @apiResourceModel App\Models\CurrencyService
+     * @apiResourceModel App\Services\Products\CurrencyService
      * @throws Exception
      */
     public function list(): AnonymousResourceCollection
